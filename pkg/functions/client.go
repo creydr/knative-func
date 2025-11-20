b6196ce58f2be4394acdb806ec29daf13a23af76
Luke Kingland <luke@lukekingland.com>
2025-11-20 22:15:15 +0900

    e2e upgrade (#2938)
    * e2e upgrade
    
    install git server in workflow
    
    disable dapr service-to-service test
    
    only run private repo test on Linux
    
    test full script etc
    
    test failure fixes
    
    remote s2i builder test full stuffs
    
    python functions
    
    fixes
    
    update func-python
    
    e2e wait by duration
    
    wait options with extended timeouts for java runtimes
    
    import common in git-server.sh
    
    disable make buffering
    
    import common to dump-logs.sh
    
    update dump-logs.sh to handle missing binaries
    
    update common.sh to be resilient to missing binaries
    
    fix: gitlab tests (retries and skip in CI)
    
    full test target - script
    
    reenable http default matrix template
    
    support user vs system config in registry setup script
    
    e2e_test.go increase default timeout
    
    full test target
    
    add CleanFS option
    
    unified github workflow
    
    renamed env var
    
    standardized envs for int tests
    
    dedicated Podman scripts
    
    integration test name prefix
    
    registry.sh skip docker restart
    
    add missing io import to gitlab_int_test.go
    
    improve prechecks
    
    * add ARM platform to matrix tests
    
    * make namespace configurable
    
    * tweaks per review
    
    * init test additional assertions
    
    * make cluster domain configurable
    
    * minor refactor and additional PR changes

--- pkg/functions/client.go
+++ pkg/functions/client.go
@@ -355,8 +355,8 @@ func WithPipelinesProvider(pp PipelinesProvider) Option {
 // operating in a restricted environment and all functions tend to take longer
 // to start up than usual, or when the client is running functions which
 // in general take longer to start.  If a timeout is specified on the
-// function itself, that will take precidence.  Use the RunWithTimeout option
-// on the Run method to specify a timeout with precidence.
+// function itself, that will take precedence.  Use the RunWithTimeout option
+// on the Run method to specify a timeout with precedence.
 func WithStartTimeout(t time.Duration) Option {
 	return func(c *Client) {
 		c.startTimeout = t
@@ -931,10 +931,10 @@ func (c *Client) Run(ctx context.Context, f Function, options ...RunOption) (job
 
 	// timeout for this run task.
 	timeout := c.startTimeout    // client's global setting is the default
-	if f.Run.StartTimeout != 0 { // Function value, if defined, takes precidence
+	if f.Run.StartTimeout != 0 { // Function value, if defined, takes precedence
 		timeout = f.Run.StartTimeout
 	}
-	if oo.StartTimeout != 0 { // Highest precidence is an option passed to Run
+	if oo.StartTimeout != 0 { // Highest precedence is an option passed to Run
 		timeout = oo.StartTimeout
 	}
 
